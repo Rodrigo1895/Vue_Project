706feb28b1d8d69dc70806152b0ada20f8ea0c34
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2020-02-08 17:47:56 -0200

    Primeiro Commit

---
+++ project_school/src/components/Aluno/Alunos.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <titulo texto="Aluno"></titulo>
+    <div>
+      <input
+        type="text"
+        placeholder="Nome do Aluno"
+        v-model="nome"
+        @keyup.enter="addAluno()"
+      />
+      <button class="btn btnInput" @click="addAluno()">Adicionar</button>
+    </div>
+
+    <table>
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{aluno.id}}</td>
+          <td>{{ aluno.nome }} {{ aluno.sobrenome }}</td>
+          <td>
+            <button class="btn btnDanger" @click="remover(aluno)">
+              Remover
+            </button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum aluno encontrado
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from "../_shared/Titulo";
+
+export default {
+  components: {
+    Titulo
+  },
+  data() {
+    return {
+      nome: "",
+      alunos: []
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/alunos")
+      .then(res => res.json())
+      .then(alunos => (this.alunos = alunos));
+  },
+  props: {},
+  methods: {
+    addAluno() {
+      let _aluno = {
+        nome: this.nome,
+        sobrenome: ""
+      };
+
+      // adiciona no servidor
+      this.$http
+        .post("http://localhost:3000/alunos", _aluno)
+        .then(res => res.json())
+        .then(alunoRetornado => {
+          this.alunos.push(alunoRetornado);
+          this.nome = "";
+        });
+    },
+    remover(aluno) {
+      // remove no servidor
+      this.$http
+        .delete(`http://localhost:3000/alunos/${aluno.id}`)
+        .then(() => {
+          let indice = this.alunos.indexOf(aluno);
+          this.alunos.splice(indice, 1);
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  border: 0px;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #687f7f;
+  display: inline;
+}
+.btnInput {
+  border: 0px;
+  padding: 20px;
+  font-size: 1.3em;
+  display: inline;
+  background-color: rgb(116, 115, 115);
+}
+.btnInput:hover {
+  padding: 20px;
+  margin: 0px;
+  border: 0px;
+}
+</style>
