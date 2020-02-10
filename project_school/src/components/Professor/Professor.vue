b304d10f19122fe3eed3b40b8d165624579776fc
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2020-02-10 21:45:42 -0200

    Aula 49

---
+++ project_school/src/components/Professor/Professor.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <titulo texto="Professores"></titulo>
+
+    <table>
+      <thead>
+        <th>Cód.</th>
+        <th>Nome</th>
+        <th>Alunos</th>
+      </thead>
+      <tbody v-if="professores.length">
+        <tr v-for="(professor, index) in professores" :key="index">
+          <td>{{ professor.id }}</td>          
+          <router-link to="/alunos" tag="td" style="cursor: pointer">
+            {{ professor.nome }} {{ professor.sobrenome }}
+          </router-link>          
+          <td>
+            3
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum professor encontrado
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from "../_shared/Titulo";
+
+export default {
+  components: {
+    Titulo
+  },
+  data() {
+    return {
+      professores: []
+    };
+  },
+   created() {
+    this.$http
+      .get("http://localhost:3000/professores")
+      .then(res => res.json())
+      .then(professores => (this.professores = professores));
+  },
+  props: {},
+};
+</script>
+
+<style scoped></style>
---
+++ project_school/src/components/Sobre/Sobre.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <titulo texto="Sobre"></titulo>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio ea necessitatibus impedit ratione nulla recusandae labore aspernatur tenetur, laudantium iusto itaque fugit, cumque maxime ducimus adipisci voluptas asperiores, porro deserunt.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque debitis quae rerum aut numquam inventore sequi quia hic! Aut, esse? Ullam inventore voluptate sint debitis praesentium vero dolores maiores harum.</p>
+  </div>
+</template>
+
+<script>
+import Titulo from "../_shared/Titulo";
+
+export default {
+    components: {
+        Titulo
+    }
+};
+</script>
+
+<style scoped></style>
